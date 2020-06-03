a2978b9e018b7a39a2f2a9f210efd74d02359a88
vierge <marancholia@gmail.com>
2020-06-03 01:06:48 +0000

    fixed syntax

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: process.env.HTTP_AUTH_NAME, password: process.env.HTTP_AUTH_PASS, except: :no_auth 
+  http_basic_authenticate_with name: env[HTTP_AUTH_NAME], password: env[HTTP_AUTH_PASS], except: :no_auth 
 
   def no_auth
     render plain: "YOU CANT SEE ME NERD"
